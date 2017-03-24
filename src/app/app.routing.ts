979f8aa68d9184889b4c12c92f878f06aa6139d9
Sam Verschueren <sam.verschueren@gmail.com>
2017-03-24 18:16:38 +0100

    init

---
+++ src/app/app.routing.ts
@@ -0,0 +1,13 @@
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent }
+];
+
+export const routing = RouterModule.forRoot(routes);
